c670f5d3c1e8edc7026683bc7d09c6f48cc78c99
langskydev <glngrizky685@gmail.com>
2024-11-10 11:22:54 +0700

    memperbarui id grub

--- utils/constants.js
+++ utils/constants.js
@@ -1,8 +1,8 @@
 // utils/constants.js
-const GROUP_ID = '120363336251818783@g.us';
+const GROUP_ID = '120363348122136023@g.us';
 const ADMIN_ID = '6285714608649';
 const allowedNumber = '6285714608649';
-const EXCLUDED_GROUP_IDS = ['120363336251818783@g.us', '120363348122136023@g.us']; // Ganti dengan ID grup Anda
+const EXCLUDED_GROUP_IDS = ['120363348122136023@g.us', '120363348122136023@g.us']; // Ganti dengan ID grup Anda
 
 module.exports = {
     GROUP_ID,
